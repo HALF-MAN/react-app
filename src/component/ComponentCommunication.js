99ac83de9c26f79f24c24528a33be4612eeee492
zhangyachong <zhangyachong@meituan.com>
2024-11-09 13:24:34 +0800

    1

---
+++ src/component/ComponentCommunication.js
@@ -0,0 +1,19 @@
+
+
+function Article() {
+    return (
+        <div>
+            <h2>标题1</h2>
+            <p>内容</p>
+        </div>
+    )
+}
+
+export default function DocApp() {
+    return (
+        <>
+            <Article />
+        </>
+
+    )
+}
